777d680a639a682b67f063eb57341cb644960835
Jack McCarthy <JackM@zirklefruit.com>
2025-12-09 16:47:52 -0800

    Adjustments made to test, toString override as well as use of the Include method to collect posts for blogs as well.

--- EFGetStarted/Model.cs
+++ EFGetStarted/Model.cs
@@ -26,8 +26,9 @@ public class Blog
 {
     public int BlogId { get; set; }
     public string Url { get; set; }
-
+    public DateTime CreatedAt { get; set; }
     public List<Post> Posts { get; } = new();
+    public override string ToString() => $"BlogId: {BlogId}, Url: {Url}, CreatedAt: {CreatedAt}, Post Count: {Posts.Count}";
 }
 
 public class Post
--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -9,23 +9,27 @@ Console.WriteLine($"Database path: {db.DbPath}.");
 
 // Create
 Console.WriteLine("Inserting a new blog");
-db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+db.Add(new Blog { Url = "http://blogs.msdn.com/adonet", CreatedAt=DateTime.UtcNow });
 await db.SaveChangesAsync();
 
 // Read
 Console.WriteLine("Querying for a blog");
-var blog = await db.Blogs
-    .OrderBy(b => b.BlogId)
-    .FirstAsync();
+
+var blog = await db.Blogs.OrderBy(b => b.BlogId).FirstAsync();
 
 // Update
 Console.WriteLine("Updating the blog and adding a post");
 blog.Url = "https://devblogs.microsoft.com/dotnet";
 blog.Posts.Add(
-    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+    new Post { Title = DateTime.UtcNow.ToString(), Content = "I wrote an app using EF Core!" });
 await db.SaveChangesAsync();
 
+await db.Blogs
+    .Include(b => b.Posts)
+    .OrderBy(b => b.BlogId)
+    .ForEachAsync(b => Console.WriteLine(b.ToString()));
+
 // Delete
-Console.WriteLine("Delete the blog");
-db.Remove(blog);
+//Console.WriteLine("Delete the blog");
+//db.Remove(blog);
 await db.SaveChangesAsync();
